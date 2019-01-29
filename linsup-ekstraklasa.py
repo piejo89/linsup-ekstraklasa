484dd6fef42889ec4e6c0a1e0280e5a43014e6c4
Przemyslaw Pawłowski <pp@alpha.home>
2019-01-29 20:32:29 +0100

    Color log.

--- linsup-ekstraklasa.py
+++ linsup-ekstraklasa.py
@@ -3,15 +3,25 @@
 import json
 import datetime
 import argparse
+from termcolor import colored, cprint
+
 
 def parse_args():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument(
-        "-p",
-        "--print",
+         "--log",
         help="Print events.",
+        dest="log",
         action="store_true"
+    )
+    parser.add_argument(
+        "-l",
+        "--limit",
+        help="Limit log entries",
+        dest="limit",
+        type=int,
+        default=0
     )
     parser.add_argument(
         "-i",
@@ -30,11 +40,12 @@ def parse_args():
     )
     return parser.parse_args()
 
+
 class Event:
-    eid = 0
+    eid_counter = 0
     def __init__(self, date, games):
-        Event.eid += 1
-        self.eid = Event.eid
+        Event.eid_counter += 1
+        self.eid = Event.eid_counter
         self.date = date
         self.games = games
         self.mistrz = self._get("mistrz")
@@ -62,53 +73,34 @@ class Event:
 
 def laod_events(data):
     events = []
-    print("======== LOADING ========")
+    #print("======== LOADING ========")
     for eid, event in data.items():
-        print("loading data for event #{}".format(eid))
+        #print("loading data for event #{}".format(eid))
         events.append(Event(event["date"], event["games"]))
-    print("========= DONE ==========")
+    #print("========= DONE ==========")
     return events
 
-def save_events(events):
-    data = {}
-    for event in events:
-        event_params= {}
-        event_params["date"] = event.date
-        event_params["games"] = event.games
-        data[event.eid] = event_params
-    with open('events.json', 'w') as file:
-        json.dump(data, file, sort_keys=True, indent=4)
 
-def get_teams(input_list):
-    result = []
-    for index1 in range(len(input_list)):
-            for index2 in range(index1+1,len(input_list)):
-                    result.append([input_list[index1],input_list[index2]])
-    return result
+def log_events(events, limit=0):
+    for event in events[-limit:]:
+        cprint(event.name, "yellow")
+        if event.mistrz is not None: cprint("Mistrz: {}".format(event.mistrz))
+        if event.pastuch is not None: cprint("Pastuch: {}".format(event.pastuch))
+        for game in event.games:
+            if game["winner"] == "team_a":
+                color_a = "green"
+                color_b = "red"
+            else:
+                color_a = "red"
+                color_b = "green"
+            vs = (colored(game["team_a"], color_a), colored("vs."), colored(game["team_b"], color_b))
+            flawless_victory = (colored("FLAWLESS VICTORY!", "red", attrs=['reverse', 'blink', 'bold']),)
+            if game["zero"]:
+                sequence = vs + flawless_victory
+            else:
+                sequence = vs
+            print(" ".join(sequence))
 
-def get_matches(input_list):
-    result = []
-    for index1 in range(len(input_list)):
-            for index2 in range(index1+1,len(input_list)):
-                    team_a = input_list[index1]
-                    team_b = input_list[index2]
-                    if not bool(set(team_a) & set(team_b)):
-                        result.append([input_list[index1],input_list[index2]])
-    return result
-
-def input_zero(prompt):
-    while True:
-        try:
-           return {"y":True,"n":False}[input(prompt).lower()]
-        except KeyError:
-           print("Invalid input please enter y or n!")
-
-def input_winner(prompt):
-    while True:
-        try:
-           return {"a":"team_a" ,"b":"team_b"}[input(prompt).lower()]
-        except KeyError:
-           print("Invalid input please enter a or b!")
 
 def add_event(date, players, events):
     teams = get_teams(players)
@@ -129,12 +121,50 @@ def add_event(date, players, events):
     events.append(Event(date, games))
 
 
-def print_events(events):
+def save_events(events):
+    data = {}
     for event in events:
-        print("======== Event: {} ========".format(event.name))
-        print("Mistrz: {}".format(event.mistrz))
-        print("Pastuch: {}".format(event.pastuch))
-        print(json.dumps(event.games, sort_keys=True, indent=2))
+        event_params= {}
+        event_params["date"] = event.date
+        event_params["games"] = event.games
+        data[Event.eid_counter] = event_params
+    with open('events.json', 'w') as file:
+        json.dump(data, file, sort_keys=True, indent=4)
+
+
+def get_teams(input_list):
+    result = []
+    for index1 in range(len(input_list)):
+            for index2 in range(index1+1,len(input_list)):
+                    result.append([input_list[index1],input_list[index2]])
+    return result
+
+
+def get_matches(input_list):
+    result = []
+    for index1 in range(len(input_list)):
+            for index2 in range(index1+1,len(input_list)):
+                    team_a = input_list[index1]
+                    team_b = input_list[index2]
+                    if not bool(set(team_a) & set(team_b)):
+                        result.append([input_list[index1],input_list[index2]])
+    return result
+
+
+def input_zero(prompt):
+    while True:
+        try:
+           return {"y":True,"n":False}[input(prompt).lower()]
+        except KeyError:
+           print("Invalid input please enter y or n!")
+
+
+def input_winner(prompt):
+    while True:
+        try:
+           return {"a":"team_a" ,"b":"team_b"}[input(prompt).lower()]
+        except KeyError:
+           print("Invalid input please enter a or b!")
 
 
 def main():
@@ -145,9 +175,8 @@ def main():
     if args.input:
         add_event(args.date, args.input, events)
         save_events(events)
-        #print_events(events)
-    if args.print:
-        print_events(events)
+    if args.log:
+        log_events(events, args.limit)
 
 
 if __name__ == "__main__":
